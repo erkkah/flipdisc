fe54c43ae8ec9231609c19c4ac151cbaec40f627
erkkah <erik.agsjo@gmail.com>
2015-12-20 17:04:16 +0100

    Updated jsdoc, added promise version of Display::open

--- main.js
+++ main.js
@@ -2,26 +2,38 @@
 
 var fs = require('fs');
 var ini = require('ini');
-var config = ini.parse(fs.readFileSync('./flipdisc.ini', 'utf-8'));
+var express = require('express');
+var http = require('http');
+var socketio = require('socket.io');
+var diskdb = require('diskdb');
+
+var FlipDisplay = require('./lib/flipdisplay')
+
+var config = ini.parse(fs.readFileSync(__dirname + '/flipdisc.ini', 'utf-8'));
 
 var PORT = config.http.port || 3000;
 var DBROOT = config.database.root || './db';
 
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var db = require('diskdb');
-db = db.connect(DBROOT);
-db.loadCollections(['displayScripts'])
+var app = express();
+var server = http.Server(app);
+var io = socketio(server);
+var db = diskdb.connect(DBROOT);
+//db.loadCollections(['displayScripts'])
 
-app.get('/', function(req, res){
-	res.sendfile('index.html');
-});
+var display = new FlipDisplay();
+
+display.open().then(function(){
+	console.log("Display opened")
+}).catch(function(err){
+	console.log("failed to init display:", err)
+})
+
+app.use(express.static(__dirname + '/public'));
 
 io.on('connection', function(socket){
 	console.log('a user connected');
 });
 
-http.listen(PORT, function(){
+server.listen(PORT, function(){
 	console.log(`listening on *:${PORT}`);
 });
