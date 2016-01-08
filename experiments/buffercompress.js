13e99d4e5469a508b5a9aaf21efcfef87c23b3dc
erkkah <erik.agsjo@gmail.com>
2016-01-09 00:18:46 +0100

    Added non-working incremental codec

--- experiments/buffercompress.js
+++ experiments/buffercompress.js
@@ -9,6 +9,8 @@ smallest addressable entity is a byte.
 
 Max bitmap size is 255 * 255.
 
+Encoding and decoding starts with a black initial "last" frame.
+
 Setup frames (0) contain width, height.
 
 Full frames (1) contain one byte per pixel stripe, performing simple RLE encoding.
@@ -16,13 +18,14 @@ Example: numwhites, numblacks, numwhites, numblacks, et.c.
 
 Note that numwhites or numwhites might be 0 for stripes longer than 255.
 
-Incremental (2) frames contain byte triplets of skip, numwhites, numblacks.
-Example: 0, 0, 22,  255, 0, 0,  26, 22, 98, ...
+Incremental (2) frames contain byte series of skip, numwhites, numblacks, numwhites, numblacks, et.c.
+terminated by a 0, 0 pair.
+
+Example: 0, 0, 22, 33, 12, 0, 0
 
 0, 0, 0 is OK, meaning no update
 
-Worst case every other pixel changes every frame, and incremental frames will be three times
-bigger than full.
+Worst case every other pixel changes every frame.
 
 Encoder will emit setup frames every X frame, full frames for initial frames or if incremental
 frames become too large.
@@ -57,6 +60,9 @@ class MonoBitmapEncoder {
 	 * @param callback {function} - will get called with (error, Buffer) on each encoding output
 	 */
 	encode(newFrame, callback){
+		assert.equal(newFrame.length, this.lastFrame.length);
+		assert(callback);
+
 		if(this.setupCounter == 0){
 			// encode setup frame here
 			var setupBuffer = [0, this.width, this.height]	// 0 == setup
@@ -65,17 +71,17 @@ class MonoBitmapEncoder {
 		this.setupCounter++;
 		this.setupCounter %= this.setupInterval;
 
-		var fullBuffer = [1];	// 1 == full
-		var incrBuffer = [2];	// 2 == incremental
+		// full frame encoding
 
 		var white = true;
 		var black = !white;
 		var mode = white;
 
+		var fullBuffer = [1];	// 1 == full
+
 		var numWhites = 0;
 		var numBlacks = 0;
-
-		// full frame encoding
+		
 		for(var pixel of newFrame){
 			if(pixel == 0){
 				if(mode == white){
@@ -93,7 +99,7 @@ class MonoBitmapEncoder {
 				}
 			}
 			else {
-				if(mode == black) {
+				if(mode == black){
 					fullBuffer.push(numBlacks);
 					numBlacks = 0;
 					mode = white;
@@ -114,12 +120,152 @@ class MonoBitmapEncoder {
 			fullBuffer.push(numWhites);
 			fullBuffer.push(0);
 		}
-		else{
+		else {
 			fullBuffer.push(numBlacks);
+		}
+
+
+		// incremental encoding
+
+		var incrBuffer = [2];	// 2 == incremental
+
+		// Initialize state for the loop below to the inverse to trigger a state
+		// change when we enter the loop.
+		var equal = newFrame[0] != this.lastFrame[0];
+		var numEqual = 0;
+
+		// Reset to collecting white changed pixels
+		mode = white;
+		numWhites = 0;
+		numBlacks = 0;
+
+		for(var i = 0; i < newFrame.length; i++){
+			var pixel = newFrame[i];
+			var lastPixel = this.lastFrame[i];
+
+			if(pixel != lastPixel){
+				if(equal){
+					// state change
+					incrBuffer.push(numEqual);
+					numEqual = 0;
+					equal = false;
+
+					// changed pixel is black
+					if(pixel == 0){
+						incrBuffer.push(0);	// numWhites
+						numBlacks++;
+						mode = black;
+					}
+					else {
+						numWhites++;
+						mode = white;
+					}
+				}
+				else {
+					if(pixel == 0){
+						// mode switch
+						if(mode == white){
+							incrBuffer.push(numWhites);
+							numWhites = 0;
+							mode = black;
+						}
+						numBlacks++;
+
+						// flush
+						if(numBlacks == 255){
+							incrBuffer.push(255);	// numBlacks
+							incrBuffer.push(0);		// numWhites
+							numBlacks = 0;
+						}
+					}
+					else {
+						if(mode == black){
+							incrBuffer.push(numBlacks);
+							numBlacks = 0;
+							mode = white;
+						}
+						numWhites++;
+
+						// flush
+						if(numWhites == 255){
+							incrBuffer.push(255);	// numWhites
+							incrBuffer.push(0);		// numBlacks
+							numWhites = 0;
+						}
+					}
+				}
+			}
+			else {
+				// pixel is unchanged
+
+				if(!equal){
+					// mode change
+					numEqual = 1;
+
+					// Only flush if going into "equal" with collected pixels.
+					// Ugly init case handling. Could be moved out of loop.
+					if(numWhites != 0 || numBlacks != 0){
+						// flush white, black
+						if(mode == white){
+							incrBuffer.push(numWhites);
+							incrBuffer.push(0);
+
+							if(numWhites != 0){
+								// terminate
+								incrBuffer.push(0);
+								incrBuffer.push(0);
+							}
+						}
+						else {
+							incrBuffer.push(numBlacks);
+							// terminate
+							incrBuffer.push(0);
+							incrBuffer.push(0);
+						}
+					}
+					equal = true;
+				}
+				else {
+					numEqual++;
+
+					if(numEqual == 256){
+						incrBuffer.push(255);	// skip
+						incrBuffer.push(0);		// no changed whites
+						incrBuffer.push(0);		// no changed blacks
+						numEqual = 1;
+					}
+				}
+			}
+		}
+
+		// finalize incremental frame
+		if(equal){
+			incrBuffer.push(numEqual);
+			incrBuffer.push(0);
+			incrBuffer.push(0);
+		}
+		else {
+			if(mode == white){
+				incrBuffer.push(numWhites);
+				incrBuffer.push(0);
+
+				if(numWhites != 0){
+					// terminate
+					incrBuffer.push(0);
+					incrBuffer.push(0);
+				}
+			}
+			else {
+				incrBuffer.push(numBlacks);
+				// terminate
+				incrBuffer.push(0);
+				incrBuffer.push(0);
+			}
 		}
 
 		this.lastFrame = new Buffer(newFrame);
 
+		//console.log(`Incremental buffer: ${incrBuffer.length}, full buffer: ${fullBuffer.length}`)
 		if(incrBuffer.length != 1 && fullBuffer.length > incrBuffer.length){
 			return callback(null, incrBuffer);
 		}
@@ -158,11 +304,33 @@ class MonoBitmapDecoder {
 					this.frame.fill(0, fullPos, fullPos + numBlacks);
 					fullPos += numBlacks;
 				}
-				callback(null, this.frame);
+				return callback(null, this.frame);
 				break;
 			case 2:
 				// incremental frame
-				return callback(new Error("Incremental decoding not implemented"));
+
+				var fullPos = 0;
+				var pos = 1;
+				while(pos < newFrame.length){
+					var skip = newFrame[pos];
+					fullPos += skip;
+					pos++;
+
+					while(pos < newFrame.length){
+						var numWhites = newFrame[pos];
+						var numBlacks = newFrame[pos + 1];
+						pos += 2;
+
+						if(numWhites == 0 && numBlacks == 0){
+							break;
+						}
+						this.frame.fill(1, fullPos, fullPos + numWhites);
+						fullPos += numWhites;
+						this.frame.fill(0, fullPos, fullPos + numBlacks);
+						fullPos += numBlacks;
+					}
+				}
+				return callback(null, this.frame);
 				break;
 			default:
 				return callback(new Error("Invalid frame code"));
@@ -173,31 +341,63 @@ class MonoBitmapDecoder {
 /// Basic tests:
 
 function runTests(){
-	var width = 255;
-	var height = 2;
+	const logLevel = 3;
+
+	var width = 10;
+	var height = 17;
 	var white = new Buffer(width * height).fill(1);
 	var black = new Buffer(width * height).fill(0);
+
+	var A = new Buffer(black);
+	var B = new Buffer(A);
+	A.fill(1, 44);
+	B.fill(1, 46);
 
 	var encoder = new MonoBitmapEncoder(width, height);
 	var decoder = new MonoBitmapDecoder();
 
-	function checkCodec(buffer){
+	function indexedCheck(bufA, bufB){
+		assert(bufA.length == bufB.length);
+		for(var i = 0; i < bufA.length; i++){
+			if(bufA[i] != bufB[i]){
+				console.log(`(${bufA[i]} != ${bufB[i]})@${i}`)
+				console.log(`A[${i}]:`, bufA.slice(i));
+				console.log(`B[${i}]:`, bufB.slice(i));
+				assert(false);
+			}
+		}
+	}
+
+	function checkCodec(buffer, logLevel){
 		encoder.encode(buffer, function(error, encoded){
 			assert.ifError(error);
-			decoder.decode(encoded, function(error, decoded){
-				assert.ifError(error);
-				assert.deepEqual(decoded, buffer);
 
+			if(logLevel > 1){
 				var ratio = 100 * (encoded.length / buffer.length);
 				console.log(`Compression ratio: ${ratio.toFixed(2)}%`);
+			}
+			if(logLevel > 2){
+				console.log("Raw: ", buffer);
+				console.log("Encoded: ", encoded);
+			}
+
+			decoder.decode(encoded, function(error, decoded){
+				assert.ifError(error);
+				//assert.deepEqual(decoded, buffer);
+				indexedCheck(decoded, buffer);
 			});
 		});
 	}
 
-	console.log("White");
-	checkCodec(white);
-	console.log("Black");
-	checkCodec(black);
+	console.log("*** White");
+	checkCodec(white, logLevel);
+
+	console.log("*** Black");
+	checkCodec(black, logLevel);
+
+	console.log("*** A/B");
+	checkCodec(A, logLevel);
+	checkCodec(B, logLevel);
 
 	function createRandom(){
 		let buffer = new Buffer(width * height);
@@ -207,9 +407,25 @@ function runTests(){
 		return buffer;
 	}
 
-	var rnd = createRandom();
-	console.log("Random");
-	checkCodec(rnd);
+	console.log("*** Random");
+	for(var i = 0; i < 10; i++){
+		var rnd = createRandom();
+		checkCodec(rnd, logLevel);
+	}
+
+	function addRandomDots(buffer) {
+		var spread = 12;
+		var pos = Math.floor(Math.random() * (buffer.length - spread));
+		var len = Math.floor(Math.random() * spread);
+		buffer.fill(1, pos, pos + len);
+	}
+
+	console.log("*** Random dots");
+	for(var i = 0; i < 1000; i++){
+		addRandomDots(black);
+		checkCodec(black, logLevel);
+	}
+
 }
 
 runTests();
